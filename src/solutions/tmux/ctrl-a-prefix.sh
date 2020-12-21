381d4ec2da5104d308cf3755e5f74ba69d0fe7f0
gtsop <gtsop+github@protonmail.com>
2020-12-21 15:42:37 +0000

    Create solution for tmux Ctrl+A binding

---
+++ src/solutions/tmux/ctrl-a-prefix.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env bash
+
+function solution() {
+	echo "###";
+	echo "# Solution: tmux/ctrl-a-prefix.sh";
+	echo "###";
+	echo "set -g prefix C-a";
+	echo "unbind C-b";
+	echo "bind C-a send-prefix";
+}
+
+solution > ~/.tmux.conf

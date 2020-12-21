708e7cdef281c80de049e0cec6f3940d27013690
gtsop <gtsop+github@protonmail.com>
2020-12-21 13:59:40 +0000

    Fix the alacritty instalation for ubuntu

--- src/solutions/install-alacritty.sh
+++ src/solutions/install-alacritty.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/env bash
 
-piu cask install alacritty
+set -e
+
+# Install Ubuntu depedencies
+piu install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
+
+# Install alacritty via cargo
+cargo install alacritty
+
+# Install desktop file for Ubuntu
+exec_path=$(which alacritty)
+icon_path=$(locate alacritty.ico)
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+file_path="$parent_path/assets/alacritty/alacritty.desktop"
+temp_dir=`mktemp -d`
+temp_file_path="$temp_dir/alacritty.desktop"
+
+cat $file_path | sed "s%<EXEC>%$exec_path%" | sed "s%<ICON>%$icon_path%" > $temp_file_path
+
+sudo desktop-file-install $temp_file_path
+
+rm -rf $temp_dir
